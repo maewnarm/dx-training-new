c5a16f72a83d6a3b83aebc4e898d65d3154dfe0e
maewnarm-ubuntu-home <vorametz@gmail.com>
2021-11-15 13:24:56 +0700

    day 1

--- src/pages/informations/button.tsx
+++ src/pages/informations/button.tsx
@@ -1,8 +1,57 @@
+import { ChangeEvent, useState,useEffect } from "react"
+
 const Button = () => {
+    const [count, setCount] = useState<number>(0)
+    const [machineName, setMachineName] = useState("initial")
+    var a = 0
+    var msg = ""
+    // state name = count
+    // function control state = setCount
+    console.log("render")
+
+    function notStateClick() {
+        a += 1
+        console.log(a)
+    }
+
+    useEffect(() => {
+        console.log("reset machine name")
+        setMachineName("")
+    },[])
+
+    useEffect(() => {
+        // console.log("set effect")
+        document.title = `You clicked ${count} times`
+    }, [count])
+
+    // function addCount() {
+    //     setCount(count+1)
+    //     console.log(count)
+    //     document.title = `You clicked ${count} times`
+    //     console.log("end")
+    // }
+
+    function addMachine(e: ChangeEvent<HTMLInputElement>) {
+        const value = e.currentTarget.value
+        setMachineName(("MC_" + value).toUpperCase())
+    }
+
+
     return (
-        <button>
-            Click me
-        </button>
+        <div className="button">
+            <button onClick={() => setCount(count + 1)}>
+            {/* <button onClick={() => addCount()}> */}
+                Click me (count)
+            </button>
+            <button onClick={notStateClick}>
+                Click me (a)
+            </button>
+            <h1>count = {count}</h1>
+            <h1>a = {a}</h1>
+            <label>Machine name</label>
+            <input type="text" onChange={(e) => addMachine(e)} />
+            <p>machine : {machineName}</p>
+        </div>
     )
 }
 
